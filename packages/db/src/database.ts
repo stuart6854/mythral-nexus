a1338ca182b0dccaafc11c3c67758ed86299fb2e
Stuart Millman <stu.millman15@gmail.com>
2025-09-17 14:15:39 +0100

    refactor: export

--- packages/db/src/database.ts
+++ packages/db/src/database.ts
@@ -2,8 +2,6 @@ import { PrismaClient } from './generated/prisma/index.js';
 
 export const prisma = new PrismaClient();
 
-async function getProjects() {
+export async function getProjects() {
   return prisma.project.findMany();
 }
-
-export { getProjects };
