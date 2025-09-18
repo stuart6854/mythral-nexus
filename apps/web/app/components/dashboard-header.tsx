d8c92d80c386a24ef35b834c41738f506e3cb22f
Stuart Millman <stu.millman15@gmail.com>
2025-09-18 12:08:43 +0100

    remove unused components

--- apps/web/app/components/dashboard-header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function DashboardHeader() {
-  return (
-    <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
-      Project Name
-    </header>
-  );
-}
