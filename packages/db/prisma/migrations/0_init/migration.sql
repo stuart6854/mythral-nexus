779e78065c3f60e3401219868e580857e9ec3972
Stuart Millman <stu.millman15@gmail.com>
2025-09-19 13:57:08 +0100

    db initial migration

---
+++ packages/db/prisma/migrations/0_init/migration.sql
@@ -0,0 +1,86 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "public"."Workspace" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Workspace_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "workspaceId" TEXT,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Resource" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "packageId" TEXT[],
+
+    CONSTRAINT "Resource_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Package" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+
+    CONSTRAINT "Package_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."_PackageToResource" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_PackageToResource_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Workspace_id_key" ON "public"."Workspace"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Workspace_name_key" ON "public"."Workspace"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_id_key" ON "public"."Project"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_name_key" ON "public"."Project"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Resource_name_key" ON "public"."Resource"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Package_name_key" ON "public"."Package"("name");
+
+-- CreateIndex
+CREATE INDEX "_PackageToResource_B_index" ON "public"."_PackageToResource"("B");
+
+-- AddForeignKey
+ALTER TABLE "public"."Project" ADD CONSTRAINT "Project_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "public"."Workspace"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Resource" ADD CONSTRAINT "Resource_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "public"."Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Package" ADD CONSTRAINT "Package_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "public"."Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_PackageToResource" ADD CONSTRAINT "_PackageToResource_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Package"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_PackageToResource" ADD CONSTRAINT "_PackageToResource_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."Resource"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
