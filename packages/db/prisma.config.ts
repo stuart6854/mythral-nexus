0e76216d269942a7ac8d5d6730e06f41934a5894
Stuart Millman <stu.millman15@gmail.com>
2025-09-19 11:15:08 +0100

    prisma changes

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,5 +1,7 @@
 import type { PrismaConfig } from 'prisma/config';
 
+import 'dotenv/config';
+
 export default {
-  schema: './prisma/schema.prisma',
+  schema: 'prisma/schema.prisma',
 } satisfies PrismaConfig;
